826bb28423a0335f3aae657e25ea4a766d27046c
mendozabernillanilsen10 <nilsenray10@gmail.com>
2024-07-30 23:21:54 -0500

    s

--- src/components/chat/ChatMessage.vue
+++ src/components/chat/ChatMessage.vue
@@ -1,10 +1,15 @@
 <template>
+  <div class="flex-1 overflow-y-auto p-4">
+    <div class="flex flex-col space-y-2">
       <ChatBubble  
+        v-for="message in messages" 
         :key="message.id"
         :ismine="message.isMine"
         :message="message.message"
+        :image="message.image"
       />
     </div>
+  </div>
 </template>
 
 <script setup lang="ts">
